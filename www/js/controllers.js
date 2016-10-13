8864261862c05a07ca6c9779a508474a2e63b5a3
gunsha <gonzaller@gmail.com>
2016-10-12 21:10:46 -0300

    background push

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -44,11 +44,11 @@ angular.module('micupon.controllers', [])
             .then(function(res) {
                 $rootScope.user.perfil = res.data[0];
                 Stamplay.Object("usuarios").patch($rootScope.user.perfil.id, {push_token:$rootScope.push_token})
-                    .then(function(res) {
+                .then(function(res) {
                       // success
-                    }, function(err) {
+                  }, function(err) {
                       // error
-                    })
+                  })
             }, function(err) {
                         // Error
                     });
@@ -86,28 +86,28 @@ angular.module('micupon.controllers', [])
     {nombre: 'Bares', imagen:'ico_comida9'}
     ];
 }])
-.controller('MapaCtrl', ['$scope','$rootScope','$cordovaGeolocation','$ionicLoading','$ionicPopup', function(s,r,$cordovaGeolocation,$ionicLoading,$ionicPopup) {
+.controller('MapaCtrl', ['$scope','$rootScope','$cordovaGeolocation','$ionicLoading','$ionicPopup','$http', function(s,r,$cordovaGeolocation,$ionicLoading,$ionicPopup,$http) {
   s.location = $cordovaGeolocation;
-    s.mapCreated = function(map) {
-        s.map = map;
-    };
-    s.iniciarMapa = function($element) {
+  s.mapCreated = function(map) {
+    s.map = map;
+};
+s.iniciarMapa = function($element) {
 	var mapOptions = {
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true
-        };
-        s.map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
-        s.centrarMapa();				 
-	}
-    s.circulo = function(marker) {
-                var sunCircle = {
-                    strokeColor: "#62B2FC",
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#62B2FC",
-                    fillOpacity: 0.35,
-                    map: s.map,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+    s.map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+    s.centrarMapa();				 
+}
+s.circulo = function(marker) {
+    var sunCircle = {
+        strokeColor: "#62B2FC",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#62B2FC",
+        fillOpacity: 0.35,
+        map: s.map,
                     radius: 500 // in meters
                 };
                 if (s.cityCircle) {
@@ -116,7 +116,7 @@ angular.module('micupon.controllers', [])
                 s.cityCircle = new google.maps.Circle(sunCircle);
                 s.cityCircle.bindTo('center', marker, 'position');
             }
-    s.centrarMapa = function() {
+            s.centrarMapa = function() {
         /*$ionicLoading.show({
             template: 'Cargando...'
         });*/
@@ -158,61 +158,65 @@ angular.module('micupon.controllers', [])
     s.localesCercanosMarker = [];
     s.nombreLocal =[];
     s.consultarLocales = function(dist) {
-                $ionicLoading.show({
-                    template: 'Buscando...'
-                });
-                Stamplay.Query('object', 'locales')
-                    .near('Point', [s.currPos.lng, s.currPos.lat], dist)
-                    .exec().then(function(res) {
-                        s.localesCercanos = res.data;
-                        s.removeLocalesMarkers();
-                        for (var i = 0; i < res.data.length; i++) {
-                            var coord = res.data[i]._geolocation.coordinates;
-                            s.nombreLocal[i] = res.data[i].nombre;
-                            console.log(i+" "+s.nombreLocal[i]);
-                            s.localesCercanosMarker.push(new google.maps.Marker({
-                                position: new google.maps.LatLng(coord[1], coord[0]),
-                                map: s.map,
-                                title: res.data[i].nombre,
-                                array_pos: i
-                            }));
+        $ionicLoading.show({
+            template: 'Buscando...'
+        });
+        Stamplay.Query('object', 'locales')
+        .near('Point', [s.currPos.lng, s.currPos.lat], dist)
+        .exec().then(function(res) {
+            s.localesCercanos = res.data;
+            s.removeLocalesMarkers();
+            for (var i = 0; i < res.data.length; i++) {
+                var coord = res.data[i]._geolocation.coordinates;
+                s.nombreLocal[i] = res.data[i].nombre;
+                console.log(i+" "+s.nombreLocal[i]);
+                s.localesCercanosMarker.push(new google.maps.Marker({
+                    position: new google.maps.LatLng(coord[1], coord[0]),
+                    map: s.map,
+                    title: res.data[i].nombre,
+                    array_pos: i
+                }));
 
-                            s.localesCercanosMarker[i].addListener('click', function() {
-                                s.localSeleccionado = s.localesCercanos[this.array_pos];
-                                s.coord = s.localSeleccionado._geolocation.coordinates;
-                                var contentString = '<div id="content">'+
-                                  '<div id="siteNotice">'+
-                                  '</div>'+
-                                  '<b>'+s.localSeleccionado.nombre+'</b>'+
-                                  '<p>'+'Comercio asociado MiCupon'+'</p>'+
-                                  '<p>'+'Cupones disponibles: '+ '<b> 1 </b>'+'</p>'+
-                                  '</div>';
-                                var infowindow = new google.maps.InfoWindow({
-                                content: contentString
-                                });
-                                console.log(this.array_pos);
-                                infowindow.open(s.map, s.localesCercanosMarker[this.array_pos]);
-                              });
-                            
-                        }
-                        if(res.data.length > 0){
-                            $ionicPopup.alert({
-                             title: 'Se encontraron ofertas cercanas!',
-                             buttons: [{
-                                text: 'Aceptar',
-                                type: 'button-positive'
-                              }]
-                           });
-                        }
-
-                        $ionicLoading.hide();
+                s.localesCercanosMarker[i].addListener('click', function() {
+                    s.localSeleccionado = s.localesCercanos[this.array_pos];
+                    s.coord = s.localSeleccionado._geolocation.coordinates;
+                    var contentString = '<div id="content">'+
+                    '<div id="siteNotice">'+
+                    '</div>'+
+                    '<b>'+s.localSeleccionado.nombre+'</b>'+
+                    '<p>'+'Comercio asociado MiCupon'+'</p>'+
+                    '<p>'+'Cupones disponibles: '+ '<b> 1 </b>'+'</p>'+
+                    '</div>';
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString
                     });
+                    console.log(this.array_pos);
+                    infowindow.open(s.map, s.localesCercanosMarker[this.array_pos]);
+                });
+
+            }
+            if(res.data.length > 0){
+                $ionicPopup.alert({
+                   title: 'Se encontraron ofertas cercanas!',
+                   buttons: [{
+                    text: 'Aceptar',
+                    type: 'button-positive'
+                }]
+            });
+                $http.post('https://micupon.stamplayapp.com/api/codeblock/v1/run/pushcercanos',{
+                    "token" : $rootScope.push_token,
+                    "mensaje" : "Se encontraron ofertas cercanas!"
+                });
+            }
+
+            $ionicLoading.hide();
+        });
     }
     s.removeLocalesMarkers = function() {
-                for (var i = 0; i < s.localesCercanosMarker.length; i++) {
-                    s.localesCercanosMarker[i].setMap(null)
-                }
-                s.localesCercanosMarker = [];
+        for (var i = 0; i < s.localesCercanosMarker.length; i++) {
+            s.localesCercanosMarker[i].setMap(null)
+        }
+        s.localesCercanosMarker = [];
     }
     s.checkDistance = function(lat1,lon1,lat2,lon2){
         var R = 6371; // km
@@ -222,14 +226,14 @@ angular.module('micupon.controllers', [])
         var lat2 = lat2* (Math.PI / 180);
 
         var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         return R * c;
     }
     s.coords = []
-document.addEventListener('deviceready', onDeviceReady, false);
+    document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady () {
+    function onDeviceReady () {
 
     /**
     * This callback will be executed every time a geolocation is recorded in the background.
@@ -239,23 +243,23 @@ function onDeviceReady () {
         var latLong = new google.maps.LatLng(location.latitude, location.longitude);
         s.map.setCenter(latLong);
         if (s.markerLocation) {
-                s.markerLocation.setMap(null);
-            }
-            s.markerLocation = new google.maps.Marker({
-                position: latLong,
-                map: s.map,
-                icon: 'img/UbicacionUsuario_.png'
-            });
-            s.coords.push({lat:location.latitude,long:location.longitude})
-            s.circulo(s.markerLocation);
-            if(s.checkDistance(s.coords[0].lat,s.coords[0].long,location.latitude, location.longitude) >= 0.5){
-                s.coords = [];
-                s.currPos = {
+            s.markerLocation.setMap(null);
+        }
+        s.markerLocation = new google.maps.Marker({
+            position: latLong,
+            map: s.map,
+            icon: 'img/UbicacionUsuario_.png'
+        });
+        s.coords.push({lat:location.latitude,long:location.longitude})
+        s.circulo(s.markerLocation);
+        if(s.checkDistance(s.coords[0].lat,s.coords[0].long,location.latitude, location.longitude) >= 0.5){
+            s.coords = [];
+            s.currPos = {
                 lat: location.latitude,
                 lng: location.longitude
             };
-                s.consultarLocales(500);
-            }
+            s.consultarLocales(500);
+        }
         backgroundGeolocation.finish();
     };
 
